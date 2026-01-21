d540f9bd3a7d6bd15661c4a162c95974e6fe8953
Supersonic Seven <b.rockholz@icloud.com>
2026-01-21 13:10:25 -0500

    Update eternity-milestones.js
    Nice try bud

--- src/core/secret-formula/eternity/eternity-milestones.js
+++ src/core/secret-formula/eternity/eternity-milestones.js
@@ -5,25 +5,25 @@ export const eternityMilestones = {
     pelleUseless: true
   },
   keepAutobuyers: {
-    eternities: 2,
+    eternities: 4,
     reward: "You start Eternity with all Normal Challenges complete, all normal autobuyers, and infinity broken"
   },
   autobuyerReplicantiGalaxy: {
-    eternities: 3,
+    eternities: 9,
     reward: "Unlock the Replicanti Galaxy Autobuyer"
   },
   keepInfinityUpgrades: {
-    eternities: 4,
+    eternities: 16,
     reward: "You start Eternity with all Infinity Upgrades",
     givenByPelle: () => PelleUpgrade.keepInfinityUpgrades.isBought,
     pelleUseless: true
   },
   bigCrunchModes: {
-    eternities: 5,
+    eternities: 25,
     reward: "Unlock more Big Crunch Autobuyer options"
   },
   autoEP: {
-    eternities: 6,
+    eternities: 36,
     reward: () => {
       const EPmin = getOfflineEPGain(TimeSpan.fromMinutes(1).totalMilliseconds);
       const em200 = getEternitiedMilestoneReward(TimeSpan.fromHours(1).totalMilliseconds,
@@ -38,122 +38,122 @@ export const eternityMilestones = {
     },
     activeCondition: () => (player.options.offlineProgress
       ? `Active as long as neither of the other offline milestones
-        (${formatInt(200)} or ${formatInt(1000)}) are also active`
+        (${formatInt(40000)} or ${formatInt(1000000)}) are also active`
       : ""),
   },
   autoIC: {
-    eternities: 7,
+    eternities: 49,
     reward: `You complete Infinity Challenges as soon as you unlock them,
-      and keep the Dimensional Sacrifice Autobuyer`,
+      and unlock the Dimensional Sacrifice Autobuyer`,
     pelleUseless: true
   },
   keepBreakUpgrades: {
-    eternities: 8,
+    eternities: 64,
     reward: "You start Eternity with all Break Infinity Upgrades",
     givenByPelle: () => PelleUpgrade.keepBreakInfinityUpgrades.isBought,
     pelleUseless: true
   },
   autobuyMaxGalaxies: {
-    eternities: 9,
+    eternities: 81,
     reward: "Unlock the buy max Antimatter Galaxies Autobuyer mode"
   },
   unlockReplicanti: {
-    eternities: 10,
+    eternities: 100,
     reward: "You start with Replicanti unlocked",
     givenByPelle: () => PelleUpgrade.replicantiStayUnlocked.isBought,
     pelleUseless: true
   },
   autobuyerID1: {
-    eternities: 11,
+    eternities: 121,
     reward: "Unlock the 1st Infinity Dimension Autobuyer",
     givenByPelle: () => PelleUpgrade.IDAutobuyers.isBought,
     pelleUseless: true
   },
   autobuyerID2: {
-    eternities: 12,
+    eternities: 144,
     reward: "Unlock the 2nd Infinity Dimension Autobuyer",
     givenByPelle: () => PelleUpgrade.IDAutobuyers.isBought,
     pelleUseless: true
   },
   autobuyerID3: {
-    eternities: 13,
+    eternities: 169,
     reward: "Unlock the 3rd Infinity Dimension Autobuyer",
     givenByPelle: () => PelleUpgrade.IDAutobuyers.isBought,
     pelleUseless: true
   },
   autobuyerID4: {
-    eternities: 14,
+    eternities: 196,
     reward: "Unlock the 4th Infinity Dimension Autobuyer",
     givenByPelle: () => PelleUpgrade.IDAutobuyers.isBought,
     pelleUseless: true
   },
   autobuyerID5: {
-    eternities: 15,
+    eternities: 225,
     reward: "Unlock the 5th Infinity Dimension Autobuyer",
     givenByPelle: () => PelleUpgrade.IDAutobuyers.isBought,
     pelleUseless: true
   },
   autobuyerID6: {
-    eternities: 16,
+    eternities: 256,
     reward: "Unlock the 6th Infinity Dimension Autobuyer",
     givenByPelle: () => PelleUpgrade.IDAutobuyers.isBought,
     pelleUseless: true
   },
   autobuyerID7: {
-    eternities: 17,
+    eternities: 289,
     reward: "Unlock the 7th Infinity Dimension Autobuyer",
     givenByPelle: () => PelleUpgrade.IDAutobuyers.isBought,
     pelleUseless: true
   },
   autobuyerID8: {
-    eternities: 18,
+    eternities: 324,
     reward: "Unlock the 8th Infinity Dimension Autobuyer",
     givenByPelle: () => PelleUpgrade.IDAutobuyers.isBought,
     pelleUseless: true
   },
   autoUnlockID: {
-    eternities: 25,
+    eternities: 625,
     reward: "You automatically unlock Infinity Dimensions upon reaching them"
   },
   unlockAllND: {
-    eternities: 30,
+    eternities: 900,
     reward: "Start with all Antimatter Dimensions available for purchase"
   },
   replicantiNoReset: {
-    eternities: 40,
+    eternities: 1600,
     reward: `Replicanti Galaxies no longer reset Antimatter, Antimatter Dimensions,
       Tickspeed, Dimensional Sacrifice, or Dimension Boosts`,
     pelleUseless: true
   },
   autobuyerReplicantiChance: {
-    eternities: 50,
+    eternities: 2500,
     reward: "Unlock the Replicanti Chance Upgrade Autobuyer",
     givenByPelle: () => PelleUpgrade.replicantiAutobuyers.isBought,
     pelleUseless: true
   },
   autobuyerReplicantiInterval: {
-    eternities: 60,
+    eternities: 3600,
     reward: "Unlock the Replicanti Interval Upgrade Autobuyer",
     givenByPelle: () => PelleUpgrade.replicantiAutobuyers.isBought,
     pelleUseless: true
   },
   autobuyerReplicantiMaxGalaxies: {
-    eternities: 80,
+    eternities: 6400,
     reward: "Unlock the Max Replicanti Galaxy Upgrade Autobuyer",
     givenByPelle: () => PelleUpgrade.replicantiAutobuyers.isBought,
     pelleUseless: true
   },
   autobuyerEternity: {
-    eternities: 100,
+    eternities: 10000,
     reward: "Unlock autobuyer for Eternities"
   },
   autoEternities: {
-    eternities: 200,
+    eternities: 40000,
     reward: () => {
       if (!player.options.offlineProgress) return `This milestone would generate eternities offline, but offline
         progress is currently disabled`;
       const eternities = getEternitiedMilestoneReward(TimeSpan.fromHours(1).totalMilliseconds,
-        player.eternities.gte(200));
+        player.eternities.gte(40000));
       // As far as I can tell, using templates here as Codefactor wants would lead to nested templates,
       // which seems messy to say the least.
       const realTime = PlayerProgress.seenAlteredSpeed() ? " real-time" : "";
@@ -168,12 +168,12 @@ export const eternityMilestones = {
       pelleUseless: true
   },
   autoInfinities: {
-    eternities: 1000,
+    eternities: 1000000,
     reward: () => {
       if (!player.options.offlineProgress) return `This milestone would generate infinities offline, but offline
         progress is currently disabled`;
       const infinities = getInfinitiedMilestoneReward(TimeSpan.fromHours(1).totalMilliseconds,
-        player.eternities.gte(1000));
+        player.eternities.gte(1000000));
       // eslint-disable-next-line prefer-template
       return `While offline, gain Infinities equal to ${formatPercents(0.5)}
         your best Infinities/hour this Eternity ` +
