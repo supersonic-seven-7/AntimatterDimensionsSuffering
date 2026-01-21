2df42f13589b5aed12335a6ffccc40ba1041a00c
Supersonic Seven <b.rockholz@icloud.com>
2026-01-21 12:43:12 -0500

    Update tabs.js
    Well this should be it

--- src/core/tabs.js
+++ src/core/tabs.js
@@ -165,7 +165,6 @@ class TabState {
   }
 
   toggleVisibility() {
-    if (this.id === Tabs.current.id) Tabs.current.id = 1;
     if (Tabs.all.filter(t => t.isUnlocked && t.id !== 1).every(t => t.isHidden) && this.id === 1) return;
     player.options.hiddenTabBits ^= (1 << this.id);
 
