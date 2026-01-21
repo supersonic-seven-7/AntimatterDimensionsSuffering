bbedf6fb6c62b834cd5a6be61efc1eabcbb34ba6
Supersonic Seven <b.rockholz@icloud.com>
2026-01-21 12:53:06 -0500

    Update infinity-upgrades.js
    Gained again

--- src/core/secret-formula/infinity/infinity-upgrades.js
+++ src/core/secret-formula/infinity/infinity-upgrades.js
@@ -157,14 +157,14 @@ export const infinityUpgrades = {
     id: "passiveGen",
     cost: 10,
     checkRequirement: () => InfinityUpgrade.dimboostMult.isBought,
-    description: () => `Passively generate Infinity Points ${formatInt(10)} times slower than your fastest Infinity`,
+    description: () => `Passively generate Infinity Points ${formatInt(100)} times slower than your fastest Infinity`,
     // Cutting corners: this is not actual effect, but it is totalIPMult that is displyed on upgrade
     effect: () => (Teresa.isRunning || V.isRunning || Pelle.isDoomed ? DC.D0 : GameCache.totalIPMult.value),
     formatEffect: value => {
       if (Teresa.isRunning || V.isRunning) return "Disabled in this reality";
       if (Pelle.isDoomed) return "Disabled";
       if (player.records.bestInfinity.time >= 999999999999) return "Too slow to generate";
-      return `${format(value, 2)} every ${Time.bestInfinity.times(10).toStringShort()}`;
+      return `${format(value, 2)} every ${Time.bestInfinity.times(100).toStringShort()}`;
     },
     charged: {
       description: () =>
